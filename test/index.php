4b9d0bffb29d74c34c3b397b6634f0a59d7bea0b
TheOGKtura <89979229+TheOGKtura@users.noreply.github.com>
2022-04-05 14:02:45 +0800

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" type="x-icon" href="images/tab.png">
+    <meta charset="UTF-8">
+    <title>Arellano University </title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="./main.js" defer></script>
+    <script src="https://kit.fontawesome.com/52d7b43dcf.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="images/aulogo.png" alt="">
+    </div>
+        <div class="logo-text">
+        <h1>E-Library System</h1>
+    </div>
+</div>
+<div>
+<ul class="nav-area">
+<li><a href="index.php">Home</a></li>
+<li><a href="news.php">News</a></li>
+<li><a href="booklist.php">Booklist</a></li>
+<li><a href="login.php">Login</a></li>
+<li><a href="help.php">Help</a></li>
+</ul>
+</div>
+    <div class="banner">
+        <h1>Electronic Library System</h1>
+    </div>
+    <div class="botw">
+    <h1>Books of the Week:</h1>
+    </div>
+</header>
+</body>
+</html>
